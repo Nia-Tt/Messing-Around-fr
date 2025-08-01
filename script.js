70d8aa2ed0e60b7d80902fed9570cb861a3a2687
Nia-Tt <tomlinsonnia044@gmail.com>
2025-08-01 20:30:20 +0000

    test

---
+++ script.js
@@ -0,0 +1,84 @@
+/*
+//7.30.25
+let number = 0;
+
+if(number <0){
+  console.log("The number is less than 0")
+}
+else if(number===0){
+  console.log("The number is exactly 0")
+}
+else{
+  console.log("The number is greater than 0")
+}
+
+//Prompting user for 3 numbers
+let uNum1 = parseInt(prompt("enter a number"));
+let uNum2 = parseInt(prompt("enter a number"));
+let uNum3 = parseInt(prompt("enter a number"));
+let biggest;
+let result;
+//Find the biggest using conditionals
+if(uNum1>=uNum2 && uNum1>=uNum3){
+  biggest = uNum1;
+}
+else if(uNum2>=uNum1 && uNum2>=uNum3){
+  biggest = uNum2;
+}
+else if(uNum3>=uNum1 && uNum3>uNum2){
+  biggest = uNum3;
+}
+result = biggest + " is the largest number entered by the user."
+console.log(result);
+*/
+
+// 7.31.25
+/*Prompt/Activity 1
+const dayNumber = new Date().getDay();
+let dayName;
+
+if(dayNumber === 0){
+  dayName = "Sunday";
+}
+else if(dayNumber === 1){
+  dayName = "Monday";
+}
+else if(dayNumber === 2){
+  dayName = "Tuesday";
+}
+else if(dayNumber === 3){
+  dayName = "Wedndesday"
+}
+else if(dayNumber === 4){
+  dayName = "Thursday"
+}
+else if(dayNumber ===5){
+  dayName = "Friday"
+}
+else if(dayNumber === 6){
+  dayName = "Saturday";
+}
+else{
+  dayName = "Day is undefined";
+}
+
+//Another way to do this using switch statements
+switch(dayNumber){
+  case 0:
+    dayName === "Sunday";
+    break;
+  case 1:
+    dayName === "Monday";
+    break;
+  case 2: 
+    dayName === "Tuesday";
+    break;
+  default:
+    console.log("I don't know what day it is");
+}
+
+alert("Today is " + dayName +"!");
+*/
+//Prompt/Activity 2
+let userName = prompt("Enter Name");
+document.getElementById("user-name").textContent = "Welcome " + userName + " to our website";
